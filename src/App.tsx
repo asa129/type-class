3b22468db434a4c3eebd79fc20c4e49306ef5141
asa129 <asa.jad1290@gmail.com>
2025-03-30 11:33:34 +0900

    テーブルからデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { Button } from "@chakra-ui/react";
+import { selectData } from "./lib/todo";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [response, setResponse] = useState();
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const data: any = selectData();
+    setResponse(data);
+  }, []);
+  console.log(response);
   return (
     <>
       <Button colorScheme="teal">ボタン</Button>
