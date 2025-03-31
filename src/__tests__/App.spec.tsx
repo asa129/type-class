89aac75b5b82ae3fe6bc2274966925a1b301d278
asa129 <asa.jad1290@gmail.com>
2025-03-31 22:10:08 +0900

    テスト追加

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,13 +1,34 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { expect, describe, test } from "@jest/globals";
 
 import App from "../App";
+import { Todo } from "../domain/todo";
+
+const mockGetAllTodos = jest
+  .fn()
+  .mockResolvedValue([
+    new Todo(1, "title1", true, "20250330"),
+    new Todo(2, "title2", true, "20250330"),
+    new Todo(3, "title3", true, "20250330"),
+    new Todo(4, "title4", true, "20250330"),
+  ]);
+
+jest.mock("../lib/todo.ts", () => ({
+  selectData: () => mockGetAllTodos(),
+}));
 
 describe("App", () => {
   test("タイトルがあること", async () => {
     render(<App />);
-    await waitFor(() => screen.getByTestId("title"));
-    const title = screen.getByTestId("title");
+    // await waitFor(() => screen.getByTestId("title"));
+    const title = await screen.findByTestId("title");
+    // const title = screen.getByTestId("title");
     expect(title).toBeInTheDocument();
+  });
+
+  test("データが4つあること", async () => {
+    render(<App />);
+    const todos = await screen.findAllByRole("row");
+    expect(todos.length - 1).toBe(4);
   });
 });
